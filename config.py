da31364f3564d486e99a28b389c51e35de4a47df
wildcathh <49352719+wildcathh@users.noreply.github.com>
2023-04-11 15:35:59 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+TILESIZE = 32
+FPS = 60
+
+PLAYER_LAYER = 4
+ENEMY_LAYER = 3
+BLOCK_LAYER = 2
+GROUND_LAYER = 1
+
+PLAYER_SPEED = 3
+
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B..E...............B',
+    'B..................B',
+    'B....BBB...........B',
+    'B..................B',
+    'B........P.........B',
+    'B..................B',
+    'B..................B',
+    'B.....BBB..........B',
+    'B.......B..........B',
+    'B.......B.....E....B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'BBBBBBBBBBBBBBBBBBBB',
+]
